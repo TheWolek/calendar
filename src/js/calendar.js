b2e72b5ac73ee0f22db279897df5e2d04c0363e4
TheWolek <pawelm916@wp.pl>
2020-06-07 23:16:46 +0200

    layout changes; select req, placeholder; year

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -31,13 +31,13 @@ function pasteMonthToSelect() {
 }
 
 function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    //currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth + 1) % 12;
     showCalendar(currentMonth, currentYear);
 }
 
 function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    //currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
     currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
     showCalendar(currentMonth, currentYear);
 }
